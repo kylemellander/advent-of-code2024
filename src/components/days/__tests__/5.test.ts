c8f0afbc618904855beee789123c6980bd5933a5
Kyle Mellander <kylemellander@gmail.com>
2024-12-04 23:17:10 -0800

    day 5

---
+++ src/components/days/__tests__/5.test.ts
@@ -0,0 +1,12 @@
+import { part1, part2 } from "../5"
+import { describe, it, expect } from "vitest"
+
+describe("test", () => {
+  it("runs part1", () => {
+    expect(part1()).toEqual(4689)
+  })
+
+  it("runs part2", () => {
+    expect(part2()).toEqual(6336)
+  })
+})
