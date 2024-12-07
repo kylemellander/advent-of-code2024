1d200223f9911f0190ce3b6702dcb3a4e47ba3ff
Kyle Mellander <kylemellander@gmail.com>
2024-12-06 21:32:34 -0800

    day7

--- src/components/days/7/index.test.ts
+++ src/components/days/7/index.test.ts
@@ -1,23 +1,12 @@
 import { part1, part2 } from "."
 import { describe, it, expect } from "vitest"
 
-const testInput = ``
-
 describe("test", async () => {
-  describe.skip("examples", () => {
-    it("runs part1 for example", () => {
-      expect(part1(testInput)).toEqual(41)
-    })
-    it("runs part2 for example", () => {
-      expect(part2(testInput)).toEqual(6)
-    })
+  it("runs part1", () => {
+    expect(part1()).toEqual(20665830408335)
   })
 
-  it.skip("runs part1", () => {
-    expect(part1()).toEqual(5329)
-  })
-
-  it.skip("runs part2", async () => {
-    expect(part2()).toEqual(2162)
+  it("runs part2", async () => {
+    expect(part2()).toEqual(354060705047464)
   })
 })
--- src/components/days/7/index.ts
+++ src/components/days/7/index.ts
@@ -1,10 +1,80 @@
 import input from "./input.txt?raw"
-import { Direction } from "../GridMap"
+
+type Equation = { result: number; numbers: number[] }
+type Operator = (a: number, b: number) => number
 
 export function part1(data = input) {
-  return data.length
+  const equations = buildEquations(data)
+  const allowedOperators = [add, multiply]
+  const validEquations = findValidEquations(equations, allowedOperators)
+  return scoreEquations(validEquations)
 }
 
 export function part2(data = input) {
-  return data.length
+  const equations = buildEquations(data)
+  const allowedOperators = [add, multiply, concat]
+  const validEquations = findValidEquations(equations, allowedOperators)
+  return scoreEquations(validEquations)
+}
+
+function buildEquations(data: string) {
+  const lines = data
+    .split("\n")
+    .map((line) => line.trim())
+    .filter(Boolean)
+  return lines.map((line) => {
+    const [resultStr, numbersStr] = line.split(": ")
+    const result = parseInt(resultStr)
+    const numbers = numbersStr.split(" ").map((n) => parseInt(n))
+    return { result, numbers }
+  })
+}
+
+function findValidEquations(
+  equations: Equation[],
+  allowedOperators: Operator[]
+) {
+  return equations.filter(({ result, numbers }) => {
+    let previousValues = [1]
+    for (let i = 0; i < numbers.length; i++) {
+      const number = numbers[i]
+      const newValues: number[] = []
+      for (const previousValue of previousValues) {
+        for (const operator of allowedOperators) {
+          const newValue = operator(previousValue, number)
+          if (newValue <= result) newValues.push(newValue)
+        }
+      }
+      previousValues = newValues
+    }
+
+    return previousValues.includes(result)
+  })
+}
+
+function scoreEquations(equations: Equation[]) {
+  let score = 0
+  for (const { result } of equations) score += result
+  return score
+}
+
+function add(a: number, b: number) {
+  return a + b
+}
+
+function multiply(a: number, b: number) {
+  return a * b
+}
+
+function concat(a: number, b: number) {
+  const aMult = roundUpToPowerOfTen(b)
+  return a * aMult + b
+}
+
+function roundUpToPowerOfTen(num: number) {
+  if (num <= 0) return 0
+  const log10 = Math.log10(num)
+  const power = Math.ceil(log10)
+  const potentialResult = Math.pow(10, power)
+  return potentialResult === num ? num * 10 : potentialResult
 }
