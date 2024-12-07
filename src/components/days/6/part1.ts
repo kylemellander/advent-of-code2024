728a2c60e2df85840e5998c7072316f228a410b3
Kyle Mellander <kylemellander@gmail.com>
2024-12-06 23:04:38 -0800

    rework day 6 part1 to use flat grid

--- src/components/days/6/part1.ts
+++ src/components/days/6/part1.ts
@@ -1,7 +1,7 @@
 import input from "./input.txt?raw"
-import { part1DataPoints } from "./shared"
+import { part1DataPointsBitwise } from "./shared"
 
 export function part1(data = input) {
-  const { positionsVisited } = part1DataPoints(data)
+  const { positionsVisited } = part1DataPointsBitwise(data)
   return Object.keys(positionsVisited).length
 }
--- src/components/days/6/shared.ts
+++ src/components/days/6/shared.ts
@@ -7,15 +7,55 @@ export const NEXT_DIRECTION = {
   W: "N" as Direction,
 }
 
-export function part1DataPoints(data: string) {
-  let position: Coord = { x: 0, y: 0 }
-  let startingPosition = ""
-  const positionsVisited = {} as Record<string, string[]>
-  let dir: Direction = "N"
+export function part1DataPointsBitwise(data: string) {
   const lines = data
     .split("\n")
     .map((n) => n.trim())
     .filter(Boolean)
+  // const height = lines.length
+  const positionsVisited = {} as Record<string, number[]>
+  const width = lines[0].length
+  const offsets = [-width, 1, width, -1]
+  const grid = lines.join("")
+  const startIndex = grid.indexOf("^")
+  let dir = 0
+  let index = startIndex
+
+  while (index >= 0 && index < grid.length) {
+    const offset = offsets[dir]
+    const { obstacle, stopAt } = findObstacleBitwise(grid, index, offset, width)
+    for (let i = index; i !== stopAt; i += offset) {
+      visitPosition(i, dir)
+    }
+    if (
+      obstacle < 0 ||
+      obstacle >= grid.length ||
+      (dir % 2 === 1 &&
+        Math.floor(obstacle / width) !== Math.floor(index / width))
+    ) {
+      visitPosition(stopAt, dir)
+      break
+    }
+    dir = (dir + 1) % 4
+    index = stopAt
+  }
+
+  function visitPosition(i: number, direction: number) {
+    positionsVisited[i] ||= []
+    positionsVisited[i].push(direction)
+  }
+
+  return { positionsVisited }
+}
+export function part1DataPoints(data: string) {
+  const lines = data
+    .split("\n")
+    .map((n) => n.trim())
+    .filter(Boolean)
+  let position: Coord = { x: 0, y: 0 }
+  let startingPosition = ""
+  const positionsVisited = {} as Record<string, string[]>
+  let dir: Direction = "N"
   const upperX = lines[0].length - 1
   const upperY = lines.length - 1
 
@@ -77,6 +117,33 @@ export function part1DataPoints(data: string) {
   }
 
   return { startingPosition, positionsVisited, upperX, upperY, obstacles }
+}
+
+function findObstacleBitwise(
+  grid: string,
+  index: number,
+  offset: number,
+  width: number
+) {
+  let obstacleIndex: number | undefined
+  let moves = 1
+  const horizontal = offset === 1 || offset === -1
+
+  while (index + offset * moves >= 0 && index + offset * moves < grid.length) {
+    if (grid[index + offset * moves] === "#") {
+      obstacleIndex = index + offset * moves
+      break
+    }
+    moves++
+    if (
+      horizontal &&
+      Math.floor((index + offset * moves) / width) !== Math.floor(index / width)
+    )
+      break
+  }
+
+  const obstacle = obstacleIndex || index + offset * moves
+  return { obstacle, stopAt: obstacle - offset }
 }
 
 function findObstacle(obstacles: Coord[], position: Coord, dir: Direction) {
