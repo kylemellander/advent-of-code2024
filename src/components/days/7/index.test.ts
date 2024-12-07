cf06eb0ab2b3ce4e3abcb70263225b427b092ccf
Kyle Mellander <kylemellander@gmail.com>
2024-12-06 20:54:31 -0800

    setup day7

---
+++ src/components/days/7/index.test.ts
@@ -0,0 +1,23 @@
+import { part1, part2 } from "."
+import { describe, it, expect } from "vitest"
+
+const testInput = ``
+
+describe("test", async () => {
+  describe.skip("examples", () => {
+    it("runs part1 for example", () => {
+      expect(part1(testInput)).toEqual(41)
+    })
+    it("runs part2 for example", () => {
+      expect(part2(testInput)).toEqual(6)
+    })
+  })
+
+  it.skip("runs part1", () => {
+    expect(part1()).toEqual(5329)
+  })
+
+  it.skip("runs part2", async () => {
+    expect(part2()).toEqual(2162)
+  })
+})
---
+++ src/components/days/7/index.ts
@@ -0,0 +1,10 @@
+import input from "./input.txt?raw"
+import { Direction } from "../GridMap"
+
+export function part1(data = input) {
+  return data.length
+}
+
+export function part2(data = input) {
+  return data.length
+}
