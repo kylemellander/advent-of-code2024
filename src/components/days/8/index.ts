4f5e684216839f7342eaca009659d599f5d38851
Kyle Mellander <kylemellander@gmail.com>
2024-12-07 23:06:34 -0800

    clean up on day 8

--- src/components/days/8/index.ts
+++ src/components/days/8/index.ts
@@ -1,60 +1,109 @@
 import input from "./input.txt?raw"
 
 export function part1(data = input) {
-  const lines = data
-    .split("\n")
-    .map((n) => n.trim())
-    .filter(Boolean)
-  const width = lines[0].length
-  const gridString = lines.join("")
-  const grid = gridString.split("")
+  const { charMap, grid, width } = setupData(data)
 
-  const uniqueChars = new Set<string>()
-  grid.forEach((char) => {
-    if (char !== ".") uniqueChars.add(char)
-  })
-
-  uniqueChars.forEach((char: string) => {
-    const indices = allIndicies(gridString, char)
+  Object.keys(charMap).forEach((char) => {
+    const indices = charMap[char]
     for (let a = 0; a < indices.length; a++) {
       for (let b = a + 1; b < indices.length; b++) {
         const indexA = indices[a]
         const indexB = indices[b]
         const distance = indexB - indexA
-        const indexAVert = Math.floor(indexA / width)
-        const indexBVert = Math.floor(indexB / width)
-        const lowerAntinode = indexA - distance
-        const upperAntinode = indexB + distance
-        const lowerAntinodeVert = Math.floor(lowerAntinode / width)
-        const upperAntinodeVert = Math.floor(upperAntinode / width)
+        const indexAY = findY(indexA, width)
+        const indexBY = findY(indexB, width)
+        const yChange = indexBY - indexAY
+        // travel upward
+        travelOut({
+          index: indexA,
+          distance: -distance,
+          width,
+          isValid: ({ index, y }) => !!grid[index] && yChange === indexAY - y,
+          onReached: (index) => {
+            if (grid[index] !== "#") grid[index] = "#"
+          },
+        })
+        // travel downward
+        travelOut({
+          index: indexB,
+          distance,
+          width,
+          isValid: ({ index, y }) => !!grid[index] && yChange === y - indexBY,
+          onReached: (index) => {
+            if (grid[index] !== "#") grid[index] = "#"
+          },
+        })
+      }
+    }
+  })
 
-        if (
-          grid[lowerAntinode] !== undefined &&
-          grid[lowerAntinode] !== "#" &&
-          indexBVert - indexAVert === indexAVert - lowerAntinodeVert
-        ) {
-          grid[lowerAntinode] = "#"
+  // console.log(joinGroupsOfN(grid, width).join("\n"))
+  return countGrid(grid)
+}
+
+export function part2(data = input) {
+  const { charMap, grid, width } = setupData(data)
+
+  Object.keys(charMap).forEach((char) => {
+    const indices = charMap[char]
+    if (indices.length > 1) {
+      indices.forEach((i) => grid[i] !== "#" && (grid[i] = "#"))
+    }
+    for (let a = 0; a < indices.length; a++) {
+      for (let b = a + 1; b < indices.length; b++) {
+        const indexA = indices[a]
+        const indexB = indices[b]
+        const distance = indexB - indexA
+        const indexAY = findY(indexA, width)
+        const indexBY = findY(indexB, width)
+        const yChange = indexBY - indexAY
+
+        // travel upward
+        let lastUpIndex = indexA
+        let lastUpIndexY = indexAY
+        while (true) {
+          const result = travelOut({
+            index: lastUpIndex,
+            distance: -distance,
+            width,
+            isValid: ({ index, y }) =>
+              !!grid[index] && yChange === lastUpIndexY - y,
+            onReached: (index) => {
+              if (grid[index] !== "#") grid[index] = "#"
+            },
+          })
+          if (!result) break
+          lastUpIndex = result.index
+          lastUpIndexY = result.y
         }
-        if (
-          grid[upperAntinode] !== undefined &&
-          grid[upperAntinode] !== "#" &&
-          indexBVert - indexAVert === upperAntinodeVert - indexBVert
-        ) {
-          grid[upperAntinode] = "#"
+
+        // travel downward
+        let lastDownIndex = indexA
+        let lastDownIndexY = indexAY
+        while (true) {
+          const result = travelOut({
+            index: lastDownIndex,
+            distance,
+            width,
+            isValid: ({ index, y }) =>
+              !!grid[index] && yChange === y - lastDownIndexY,
+            onReached: (index) => {
+              if (grid[index] !== "#") grid[index] = "#"
+            },
+          })
+          if (!result) break
+          lastDownIndex = result.index
+          lastDownIndexY = result.y
         }
       }
     }
   })
 
   // console.log(joinGroupsOfN(grid, width).join("\n"))
-  let count = 0
-  for (let i = 0; i < grid.length; i++) {
-    if (grid[i] === "#") count++
-  }
-  return count
+  return countGrid(grid)
 }
 
-export function part2(data = input) {
+function setupData(data: string) {
   const lines = data
     .split("\n")
     .map((n) => n.trim())
@@ -73,59 +122,7 @@ export function part2(data = input) {
     charMap[char] = allIndicies(gridString, char)
   })
 
-  Object.keys(charMap).forEach((char) => {
-    const indices = charMap[char]
-    if (indices.length > 1) {
-      indices.forEach((i) => grid[i] !== "#" && (grid[i] = "#"))
-    }
-    for (let a = 0; a < indices.length; a++) {
-      for (let b = a + 1; b < indices.length; b++) {
-        const indexA = indices[a]
-        const indexB = indices[b]
-        const distance = indexB - indexA
-        const indexAVert = Math.floor(indexA / width)
-        const indexBVert = Math.floor(indexB / width)
-        const vertSteps = indexBVert - indexAVert
-        let lastLowerAntinode = indexA
-        let lastLowerAntinodeVert = indexAVert
-        while (true) {
-          const lowerAntinode = lastLowerAntinode - distance
-          const lowerAntinodeVert = Math.floor(lowerAntinode / width)
-          if (
-            grid[lowerAntinode] === undefined ||
-            vertSteps !== lastLowerAntinodeVert - lowerAntinodeVert
-          ) {
-            break
-          }
-          if (grid[lowerAntinode] !== "#") grid[lowerAntinode] = "#"
-          lastLowerAntinode = lowerAntinode
-          lastLowerAntinodeVert = lowerAntinodeVert
-        }
-        let lastUpperAntinode = indexB
-        let lastUpperAntinodeVert = indexBVert
-        while (true) {
-          const upperAntinode = lastUpperAntinode + distance
-          const upperAntinodeVert = Math.floor(upperAntinode / width)
-          if (
-            grid[upperAntinode] === undefined ||
-            vertSteps !== upperAntinodeVert - lastUpperAntinodeVert
-          ) {
-            break
-          }
-          if (grid[upperAntinode] !== "#") grid[upperAntinode] = "#"
-          lastUpperAntinode = upperAntinode
-          lastUpperAntinodeVert = upperAntinodeVert
-        }
-      }
-    }
-  })
-
-  // console.log(joinGroupsOfN(grid, width).join("\n"))
-  let count = 0
-  for (let i = 0; i < grid.length; i++) {
-    if (grid[i] === "#") count++
-  }
-  return count
+  return { charMap, grid, width }
 }
 
 function allIndicies(str: string, char: string): number[] {
@@ -134,6 +131,38 @@ function allIndicies(str: string, char: string): number[] {
     if (str[i] === char) indices.push(i)
   }
   return indices
+}
+
+function findY(index: number, width: number) {
+  return Math.floor(index / width)
+}
+
+function travelOut({
+  index,
+  distance,
+  width,
+  isValid,
+  onReached,
+}: {
+  index: number
+  distance: number
+  width: number
+  isValid(args: { index: number; y: number }): boolean
+  onReached: (index: number) => void
+}): false | { index: number; y: number } {
+  const i = index + distance
+  const y = findY(i, width)
+  if (!isValid({ index: i, y })) return false
+  onReached(i)
+  return { index: i, y }
+}
+
+function countGrid(grid: string[]) {
+  let count = 0
+  for (let i = 0; i < grid.length; i++) {
+    if (grid[i] === "#") count++
+  }
+  return count
 }
 
 export function joinGroupsOfN(arr: string[], n: number): string[] {
