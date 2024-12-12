1b9353b4adf0eabf8becf6f1470e2b38b4e9dd80
Kyle Mellander <kylemellander@gmail.com>
2024-12-12 10:20:18 -0800

    refactor day12

--- src/days/12/index.ts
+++ src/days/12/index.ts
@@ -1,18 +1,10 @@
 import input from "./input.txt?raw"
+import { getNeighborIndexes, getNeighbors, setupMap } from "./shared"
 export { part2 } from "./part2"
 
 export function part1(data = input) {
-  const lines = data
-    .split("\n")
-    .map((line) => line.trim())
-    .filter(Boolean)
-  const width = lines[0].length
-  const length = lines.length * width
-  const offsets = [-width, 1, width, -1]
-  const map = lines.join("")
-
+  const { length, offsets, map, width } = setupMap(data)
   const usedIndexes = new Set<number>()
-
   let cost = 0
 
   for (let i = 0; i < length; i++) {
@@ -21,17 +13,19 @@ export function part1(data = input) {
     usedIndexes.add(i)
 
     for (let j = 0; j < plot.length; j++) {
-      getNeighbors(plot[j]).forEach((neighbor) => {
-        if (usedIndexes.has(neighbor)) return
-        usedIndexes.add(neighbor)
-        plot.push(neighbor)
-      })
+      getNeighbors({ index: plot[j], map, width, offsets }).forEach(
+        (neighbor) => {
+          if (usedIndexes.has(neighbor)) return
+          usedIndexes.add(neighbor)
+          plot.push(neighbor)
+        }
+      )
     }
 
     const area = plot.length
     const perimeter = plot.reduce((acc, index) => {
-      const neighbors = getNeighborIndexes(index).filter((i) =>
-        plot.includes(i)
+      const neighbors = getNeighborIndexes({ index, offsets, width }).filter(
+        (i) => plot.includes(i)
       )
       const walls = 4 - neighbors.length
       return acc + walls
@@ -41,20 +35,4 @@ export function part1(data = input) {
   }
 
   return cost
-
-  function getNeighbors(index: number) {
-    const value = map[index]
-    return getNeighborIndexes(index).filter((i) => map[i] === value)
-  }
-  function getNeighborIndexes(index: number) {
-    return offsets.map((offset) => {
-      const i = index + offset
-      if (
-        offsets.indexOf(offset) % 2 === 1 &&
-        Math.floor(i / width) !== Math.floor(index / width)
-      )
-        return -1
-      return i
-    })
-  }
 }
