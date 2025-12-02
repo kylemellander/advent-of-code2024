fefb437b4b61de7618ec45aa3c76cf1a9da97933
Kyle Mellander <kylemellander@users.noreply.github.com>
2025-12-01 21:49:39 -0800

    day 2

--- src/components/PartResults.tsx
+++ src/components/PartResults.tsx
@@ -1,28 +1,28 @@
 import { useCurrentDay } from "./useCurrentDay"
 import * as day1 from "../days/1"
 import * as day2 from "../days/2"
-import * as day3 from "../days/3"
-import * as day4 from "../days/4"
-import * as day5 from "../days/5"
-import * as day6 from "../days/6"
-import * as day7 from "../days/7"
-import * as day8 from "../days/8"
-import * as day9 from "../days/9"
-import * as day10 from "../days/10"
-import * as day11 from "../days/11"
-import * as day12 from "../days/12"
-import * as day13 from "../days/13"
-import * as day14 from "../days/14"
-import * as day15 from "../days/15"
-import * as day16 from "../days/16"
-import * as day17 from "../days/17"
-import * as day18 from "../days/18"
-import * as day19 from "../days/19"
-import * as day20 from "../days/20"
-import * as day21 from "../days/21"
-import * as day22 from "../days/22"
-import * as day23 from "../days/23"
-import * as day24 from "../days/24"
+// import * as day3 from "../days/3"
+// import * as day4 from "../days/4"
+// import * as day5 from "../days/5"
+// import * as day6 from "../days/6"
+// import * as day7 from "../days/7"
+// import * as day8 from "../days/8"
+// import * as day9 from "../days/9"
+// import * as day10 from "../days/10"
+// import * as day11 from "../days/11"
+// import * as day12 from "../days/12"
+// import * as day13 from "../days/13"
+// import * as day14 from "../days/14"
+// import * as day15 from "../days/15"
+// import * as day16 from "../days/16"
+// import * as day17 from "../days/17"
+// import * as day18 from "../days/18"
+// import * as day19 from "../days/19"
+// import * as day20 from "../days/20"
+// import * as day21 from "../days/21"
+// import * as day22 from "../days/22"
+// import * as day23 from "../days/23"
+// import * as day24 from "../days/24"
 import { useState } from "react"
 
 const map: Record<
@@ -35,28 +35,28 @@ const map: Record<
 > = {
   1: day1,
   2: day2,
-  3: day3,
-  4: day4,
-  5: day5,
-  6: day6,
-  7: day7,
-  8: day8,
-  9: day9,
-  10: day10,
-  11: day11,
-  12: day12,
-  13: day13,
-  14: day14,
-  15: day15,
-  16: day16,
-  17: day17,
-  18: day18,
-  19: day19,
-  20: day20,
-  21: day21,
-  22: day22,
-  23: day23,
-  24: day24,
+  // 3: day3,
+  // 4: day4,
+  // 5: day5,
+  // 6: day6,
+  // 7: day7,
+  // 8: day8,
+  // 9: day9,
+  // 10: day10,
+  // 11: day11,
+  // 12: day12,
+  // 13: day13,
+  // 14: day14,
+  // 15: day15,
+  // 16: day16,
+  // 17: day17,
+  // 18: day18,
+  // 19: day19,
+  // 20: day20,
+  // 21: day21,
+  // 22: day22,
+  // 23: day23,
+  // 24: day24,
 }
 
 export function PartResults({ part }: { part: 1 | 2 }) {
--- src/days/2/index.ts
+++ src/days/2/index.ts
@@ -1,9 +1,58 @@
-// import input from "./input.txt?raw"
+import input from "./input.txt?raw"
 
-export function part1() {
-
+export function part1(data: string = input) {
+  return findInvalidIdSum(data, isInvalidId)
 }
 
-export function part2() {
+export function part2(data: string = input) {
+  return findInvalidIdSum(data, isInvalidId2)
+}
 
+function findInvalidIdSum(
+  data: string,
+  idChecker: (id: string) => boolean
+): number {
+  const line = data.split("\n").filter(Boolean)[0]
+  const idRanges = line.split(",").map((cmd) => cmd.split("-"))
+
+  let sum = 0
+  for (const [start, end] of idRanges) {
+    const startNum = Number(start)
+    const endNum = Number(end)
+    for (let i = startNum; i <= endNum; i++) {
+      if (idChecker(i.toString())) {
+        sum += i
+      }
+    }
+  }
+  return sum
+}
+
+function isInvalidId(id: string): boolean {
+  const length = id.length
+  if (length % 2 !== 0) return false
+
+  const halfLength = length >> 1
+  return id.slice(0, halfLength) === id.slice(halfLength)
+}
+
+function isInvalidId2(id: string): boolean {
+  const length = id.length
+
+  for (let i = 1; i <= length / 2; i++) {
+    if (length % i !== 0) continue
+
+    const section = id.slice(0, i)
+    let matches = true
+
+    for (let j = i; j < length; j += i) {
+      if (id.slice(j, j + i) !== section) {
+        matches = false
+        break
+      }
+    }
+
+    if (matches) return true
+  }
+  return false
 }
