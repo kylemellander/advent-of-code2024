f4e95e82b3cd109d31923c550fdc2e4e806cb213
Kyle Mellander <kylemellander@users.noreply.github.com>
2025-12-02 23:28:26 -0800

    day 3

--- src/days/3/index.test.ts
+++ src/days/3/index.test.ts
@@ -2,19 +2,31 @@ import { part1, part2 } from "."
 import { describe, it, expect } from "vitest"
 
 describe("test", () => {
+  describe("example", () => {
+    it("runs part1", () => {
+      expect(
+        part1(`987654321111111
+811111111111119
+234234234234278
+818181911112111`)
+      ).toEqual(357)
+    })
+
+    it("runs part2", () => {
+      expect(
+        part2(`987654321111111
+811111111111119
+234234234234278
+818181911112111`)
+      ).toEqual(3121910778619)
+    })
+  })
+
   it("runs part1", () => {
-    expect(part1()).toEqual(173731097)
+    expect(part1()).toEqual(16927)
   })
 
   it("runs part2", () => {
-    expect(part2()).toEqual(93729253)
-  })
-
-  describe("part2", () => {
-    it("runs with example", () => {
-      const example =
-        "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
-      expect(part2(example)).toEqual(48)
-    })
+    expect(part2()).toEqual(167384358365132)
   })
 })
--- src/days/3/index.ts
+++ src/days/3/index.ts
@@ -1,9 +1,42 @@
-// import input from "./input.txt?raw"
+import input from "./input.txt?raw"
 
-export function part1(data = input) {
+export function part1(data: string = input) {
+  const lines = data.split("\n").filter(Boolean)
+  const highests = lines.map((line) => {
+    const nums = line.split("").map(Number)
 
+    const highest = Math.max(...nums.slice(0, -1))
+    const highestIndex = line.indexOf(highest.toString())
+    const remaining = nums.slice(highestIndex + 1)
+    const secondHighest = Math.max(...remaining)
+    return 10 * highest + secondHighest
+  })
+  return highests.reduce((a, b) => a + b, 0)
 }
 
-export function part2(data = input) {
+export function part2(data: string = input) {
+  const lines = data.split("\n").filter(Boolean)
+  return lines.reduce((acc, line) => {
+    return acc + parseInt(findLongJoltage(line))
+  }, 0)
+}
 
+function findLongJoltage(line: string, length = 12): string {
+  const nums = line.split("").map(Number)
+
+  let voltage = ""
+  let lastHighestIndex = 0
+  while (voltage.length < length) {
+    const range =
+      voltage.length === length - 1
+        ? nums.slice(lastHighestIndex)
+        : nums.slice(lastHighestIndex, -(length - voltage.length - 1))
+    const highest = Math.max(...range)
+    const highestIndex = range.indexOf(highest)
+    voltage = `${voltage}${highest}`
+    lastHighestIndex += highestIndex + 1
+    if (lastHighestIndex >= nums.length) break
+  }
+
+  return voltage
 }
