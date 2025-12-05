22fc3b9f0fc87967cd3b0d772c2aad6390c6559f
Kyle Mellander <kylemellander@users.noreply.github.com>
2025-12-04 21:26:28 -0800

    day 5 - extract combining ranges
    this also uses them in part1 for performance so that each id has less
    to check.

--- src/days/5/index.ts
+++ src/days/5/index.ts
@@ -2,9 +2,7 @@ import input from "./input.txt?raw"
 
 export function part1(data: string = input) {
   const [rawRanges, rawIngredientIds] = data.split("\n\n").filter(Boolean)
-  const ranges = rawRanges
-    .split("\n")
-    .map((s) => s.trim().split("-").map(Number))
+  const ranges = combineRanges(rawRanges)
   const ingredientIds = rawIngredientIds.split("\n").filter(Boolean).map(Number)
 
   const ids = []
@@ -21,23 +19,27 @@ export function part1(data: string = input) {
 
 export function part2(data: string = input) {
   const [rawRanges] = data.split("\n\n").filter(Boolean)
-  const ranges = rawRanges
-    .split("\n")
-    .map((s) => s.trim().split("-").map(Number))
-    .sort((a, b) => a[0] - b[0])
-
-  const combinedRanges: number[][] = []
-  for (const range of ranges) {
-    const lastRange = combinedRanges[combinedRanges.length - 1]
-    if (!lastRange || range[0] > lastRange[1] + 1) {
-      combinedRanges.push(range)
-    } else {
-      lastRange[1] = Math.max(lastRange[1], range[1])
-    }
-  }
+  const combinedRanges = combineRanges(rawRanges)
 
   return combinedRanges.reduce(
     (sum, [start, end]) => sum + (end - start + 1),
     0
   )
+}
+
+function combineRanges(rawRanges: string): number[][] {
+  const ranges = rawRanges
+    .split("\n")
+    .map((s) => s.trim().split("-").map(Number))
+    .sort((a, b) => a[0] - b[0])
+  const combined: number[][] = []
+  for (const range of ranges) {
+    const lastRange = combined[combined.length - 1]
+    if (!lastRange || range[0] > lastRange[1] + 1) {
+      combined.push(range)
+    } else {
+      lastRange[1] = Math.max(lastRange[1], range[1])
+    }
+  }
+  return combined
 }
